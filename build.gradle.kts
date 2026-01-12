6bf04c0b71461dbaf92f79ebe5ab5c8c89e22ecb
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2026-01-12 22:49:17 +0800

    version: 0.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val projectUrl = "https://github.com/ccbluex/$projectName"
 
 allprojects {
     group = "net.ccbluex"
-    version = "0.2.3"
+    version = "0.2.4"
 
     repositories {
         mavenLocal()
