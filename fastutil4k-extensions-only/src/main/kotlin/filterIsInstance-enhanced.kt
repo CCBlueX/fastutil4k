aa6e45fcde6fc8d0862f8ab13595265cc2a276cb
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-01-30 13:55:38 +0800

    fix spotless

--- fastutil4k-extensions-only/src/main/kotlin/filterIsInstance-enhanced.kt
+++ fastutil4k-extensions-only/src/main/kotlin/filterIsInstance-enhanced.kt
@@ -1,5 +1,6 @@
 @file:Suppress("unused", "NOTHING_TO_INLINE")
 @file:JvmName("filterIsInstance-enhanced")
+
 package net.ccbluex.fastutil
 
 import java.util.function.Predicate
@@ -8,25 +9,21 @@ import java.util.stream.Stream
 /**
  * Equivalent to `this.filterIsInstance<R>.filter { predicate(it) }`
  */
-inline fun <reified R> Array<*>.filterIsInstance(predicate: (R) -> Boolean): List<R> =
-    filterIsInstanceTo(ArrayList(), predicate)
+inline fun <reified R> Array<*>.filterIsInstance(predicate: (R) -> Boolean): List<R> = filterIsInstanceTo(ArrayList(), predicate)
 
 /**
  * Equivalent to `this.filterIsInstance<R>.filter { predicate(it) }`
  */
-inline fun <reified R> Iterable<*>.filterIsInstance(predicate: (R) -> Boolean): List<R> =
-    filterIsInstanceTo(ArrayList(), predicate)
-
-/**
- * Equivalent to `this.filterIsInstance<R>.filter { predicate(it) }`
- */
-@Suppress("UNCHECKED_CAST")
-inline fun <reified R> Sequence<*>.filterIsInstance(crossinline predicate: (R) -> Boolean): Sequence<R> =
-    filter { it is R && predicate(it) } as Sequence<R>
+inline fun <reified R> Iterable<*>.filterIsInstance(predicate: (R) -> Boolean): List<R> = filterIsInstanceTo(ArrayList(), predicate)
 
 /**
  * Equivalent to `this.filterIsInstance<R>.filter { predicate(it) }`
  */
 @Suppress("UNCHECKED_CAST")
-inline fun <reified R> Stream<*>.filterIsInstance(crossinline predicate: (R) -> Boolean): Stream<R> =
-    filter(Predicate { it is R && predicate(it) }) as Stream<R>
+inline fun <reified R> Sequence<*>.filterIsInstance(crossinline predicate: (R) -> Boolean): Sequence<R> = filter { it is R && predicate(it) } as Sequence<R>
+
+/**
+ * Equivalent to `this.filterIsInstance<R>.filter { predicate(it) }`
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified R> Stream<*>.filterIsInstance(crossinline predicate: (R) -> Boolean): Stream<R> = filter(Predicate { it is R && predicate(it) }) as Stream<R>
